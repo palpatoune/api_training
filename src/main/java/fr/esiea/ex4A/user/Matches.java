555aaab0a9e0d5c2f0f07d4505e3a7674a1478c8
palpatoune <simon.btc42@gmail.com>
2021-03-26 23:12:52 +0100

    Get et POST valide fin Partie 2

---
+++ src/main/java/fr/esiea/ex4A/user/Matches.java
@@ -0,0 +1,22 @@
+package fr.esiea.ex4A.user;
+
+public class Matches {
+    private String userName;
+    private String userTweeter;
+    private String userCountry;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserTweeter() {
+        return userTweeter;
+    }
+
+    public Matches(String userName, String userTweeter, String userCountry) {
+        this.userName = userName;
+        this.userTweeter = userTweeter;
+        this.userCountry = userCountry;
+    }
+}
+
--- src/main/java/fr/esiea/ex4A/user/UserServerController.java
+++ src/main/java/fr/esiea/ex4A/user/UserServerController.java
@@ -2,17 +2,36 @@ package fr.esiea.ex4A.user;
 
 
 
-import java.util.List;
+import java.util.*;
+import java.util.ArrayList;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
 public class UserServerController {
 
+    Matches match1 = new Matches("Bob","bobTweeter", "Fr");
+    Matches match2 = new Matches("rick","rickTweeter","Fr");
+    Matches match3 = new Matches("aucun","aucunTweeter","Fr");
+
+
+
     @GetMapping("/api/matches")
-    String match(@RequestParam(value = "userName", defaultValue = "Bobibob", required = false)String userName){
-        return userName;
+    Matches match(@RequestParam(value = "name", defaultValue = "default", required = false)String userName,
+                  @RequestParam(value = "country", defaultValue = "Eng", required = false)String userCountry){
+        System.out.println(userName);
+        System.out.println(match1.getUserName());
+        if (userName.equals(match1.getUserName())){
+             return match1;
+        }
+        if (userName.equals(match2.getUserName())){
+             return match2;
+        }
+        return match3;
+
     }
 
     @PostMapping("/api/inscription")
